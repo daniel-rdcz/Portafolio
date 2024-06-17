43cf4b3d6612c1d3aa8b55f0cc3726d09b51a577
daniel-rdcz <danield_895@hotmail.com>
2024-06-17 13:29:38 -0600

    Border Hover done

--- src/scripts/gridAnimation.js
+++ src/scripts/gridAnimation.js
@@ -2,14 +2,14 @@ document.addEventListener('DOMContentLoaded', function() {
     const wrapper = document.getElementById('wrapper');
 
     const animateOptions = {
-        duration: 500,
+        duration: 900,
         easing: 'backInOut',
     }
     
     animateCSSGrid.wrapGrid(wrapper, animateOptions)
 
     const handleResize = () => {
-        if (window.innerWidth <= 768) {
+        if (window.innerWidth <= 850) {
             wrapper.classList.add('small-screen');
         } else {
             wrapper.classList.remove('small-screen');
